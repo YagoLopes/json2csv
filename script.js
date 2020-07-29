13cc949c63a079b1beb4cfd58345b65a3e96dd38
Yago Lopes <yagolopeslazaro@gmail.com>
2020-07-29 10:05:28 -0300

    add function donwload file

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ function convertJsonInCsv() {
 
   start();
 }
+
+function clickButtonDownload() {
+  const text = document.getElementById("csv").value;
+  const name = `${new Date().getTime().toString()}.csv`;
+  downloadToFile(text, name, "text/plain");
+}
+
+const downloadToFile = (content, filename, contentType) => {
+  const a = document.createElement("a");
+  const file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(a.href);
+};
